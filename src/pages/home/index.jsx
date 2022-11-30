3ed148023ed7783628ffd11df54fac04ac2975e3
LucVanMinh <minh.lucvan.nccasia@gmail.com>
2022-11-30 22:43:15 +0700

    s

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import CustomCard from "../../components/Card/Card";
 
 const Home = () => {
   return (
     <div>
       <h2>Home </h2>
-      <div></div>
+      <div>
+        <CustomCard />
+      </div>
     </div>
   );
 };
