a0f65153805998d3d5d1fed29874cc21d0711174
duclongdev <20521566@gm.uit.edu.vn>
2022-12-04 21:48:57 +0700

    add login with fb by firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import GlobalStyle from "./global";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import GlobalStyle from './global'
+import { UserProvider } from './utils/UserProvider'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <GlobalStyle>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </GlobalStyle>
   </React.StrictMode>
-);
+)
---
+++ src/utils/UserProvider.jsx
@@ -0,0 +1,39 @@
+import { useContext, createContext, useEffect, useState } from 'react'
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth'
+import { auth } from '../firebase'
+
+const UserContext = createContext()
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({})
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithRedirect(auth, provider)
+  }
+  const logOut = () => {
+    signOut(auth)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ googleSignIn, logOut, user }}>{children}</UserContext.Provider>
+  )
+}
+
+export const UserAuth = () => {
+  return useContext(UserContext)
+}
